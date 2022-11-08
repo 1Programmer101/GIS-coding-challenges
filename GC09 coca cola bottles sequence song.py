ddcff66cb827f923fd6c11f222564c6b85adee3d
Tsunami <112594042+1Programmer101@users.noreply.github.com>
2022-11-08 23:19:31 +0800

    Create GC09 coca cola bottles sequence song.py

---
+++ GC09 coca cola bottles sequence song.py
@@ -0,0 +1,6 @@
+for bottle in range(99, 0, -1):
+   print(bottle, "bottles of cola on the wall.", bottle, "bottles of cola, take one down, pass it around.", end=" ")
+   if bottle%5 == 0:
+       print("")
+print("0 bottles of cola on the wall. 0 bottles of cola")
+
